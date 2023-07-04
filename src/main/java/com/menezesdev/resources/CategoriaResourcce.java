82190e1938a94bea44c2766f7de292fd123a76c5
Menezes18 <Gustavo.e.menezes@outlook.com>
2023-07-04 15:04:09 -0300

    Testando a primeira classe de dominío - Categoria

--- src/main/java/com/menezesdev/resources/CategoriaResourcce.java
+++ src/main/java/com/menezesdev/resources/CategoriaResourcce.java
@@ -1,18 +1,33 @@
 package com.menezesdev.resources;
 
+import com.menezesdev.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value ="/categorias")
 public class CategoriaResourcce {
 	
 	
+	
 	@RequestMapping(method=RequestMethod.GET) //obter um dado 
-	public String listar()
+	public List<Categoria> listar()
 	{
-		return "Rest esta funcionando!";
+		Categoria cat1  = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+
+		List<Categoria> listaCategoria = new ArrayList<>();
+		listaCategoria.add(cat1);
+		listaCategoria.add(cat2);
+
+
+
+		
+		return  listaCategoria;
 	}
 
 }
